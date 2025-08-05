ee881f3eb0ec1214af630ee7376743f5e4d4c15c
odisfm <102568741+odisfm@users.noreply.github.com>
2025-08-05 20:29:21 +1000

    add supported versions

---
+++ client/versionData.tsx
@@ -0,0 +1,86 @@
+export const supportedVersionGroups = [
+    {
+        name: "Red / Blue",
+        api_path: "red-blue"
+    },
+    {
+        name: "Yellow",
+        api_path: "yellow"
+    },
+    {
+        name: "Gold / Silver",
+        api_path: "gold-silver"
+    },
+    {
+        name: "Crystal",
+        api_path: "crystal"
+    },
+    {
+        name: "Ruby / Sapphire",
+        api_path: "ruby-sapphire"
+    },
+    {
+        name: "Emerald",
+        api_path: "emerald"
+    },
+    {
+        name: "FireRed / LeafGreen",
+        api_path: "firered-leafgreen"
+    },
+    {
+        name: "Diamond / Pearl",
+        api_path: "diamond-pearl"
+    },
+    {
+        name: "Platinum",
+        api_path: "platinum"
+    },
+    {
+        name: "HeartGold / SoulSilver",
+        api_path: "heartgold-soulsilver"
+    },
+    {
+        name: "Black / White",
+        api_path: "black-white"
+    },
+    {
+        name: "Black 2 / White 2",
+        api_path: "black-2-white-2"
+    },
+    {
+        name: "X / Y",
+        api_path: "x-y"
+    },
+    {
+        name: "Omega Ruby / Alpha Sapphire",
+        api_path: "omega-ruby-alpha-sapphire"
+    },
+    {
+        name: "Sun / Moon",
+        api_path: "sun-moon"
+    },
+    {
+        name: "Ultra Sun / Ultra Moon",
+        api_path: "ultra-sun-ultra-moon"
+    },
+    {
+        name: "Let's Go Pikachu / Let's Go Eevee",
+        api_path: "lets-go-pikachu-lets-go-eevee"
+    },
+    {
+        name: "Sword / Shield",
+        api_path: "sword-shield"
+    },
+    {
+        name: "Brilliant Diamond / Shining Pearl",
+        api_path: "brilliant-diamond-shining-pearl"
+    },
+    {
+        name: "Legends: Arceus",
+        api_path: "legends-arceus"
+    },
+    {
+        name: "Scarlet / Violet",
+        api_path: "scarlet-violet"
+    }
+];
