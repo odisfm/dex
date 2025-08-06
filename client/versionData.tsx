5b52d1fde772cff1d46e8ad7bc2f022cd5dbda81
odisfm <102568741+odisfm@users.noreply.github.com>
2025-08-07 07:26:54 +1000

    add detail

--- client/versionData.tsx
+++ client/versionData.tsx
@@ -1,87 +1,129 @@
 export const supportedVersionGroups = [
     {
         name: "Red / Blue",
-        api_path: "red-blue"
+        api_path: "red-blue",
+        generation: "generation-i",
+        versions: ["red", "blue"]
     },
     {
         name: "Yellow",
-        api_path: "yellow"
+        api_path: "yellow",
+        generation: "generation-i",
+        versions: ["yellow"]
     },
     {
         name: "Gold / Silver",
-        api_path: "gold-silver"
+        api_path: "gold-silver",
+        generation: "generation-ii",
+        versions: ["gold", "silver"]
     },
     {
         name: "Crystal",
-        api_path: "crystal"
+        api_path: "crystal",
+        generation: "generation-ii",
+        versions: ["crystal"]
     },
     {
         name: "Ruby / Sapphire",
-        api_path: "ruby-sapphire"
+        api_path: "ruby-sapphire",
+        generation: "generation-iii",
+        versions: ["ruby", "sapphire"]
     },
     {
         name: "Emerald",
-        api_path: "emerald"
+        api_path: "emerald",
+        generation: "generation-iii",
+        versions: ["emerald"]
     },
     {
         name: "FireRed / LeafGreen",
-        api_path: "firered-leafgreen"
+        api_path: "firered-leafgreen",
+        generation: "generation-iii",
+        versions: ["firered", "leafgreen"]
     },
     {
         name: "Diamond / Pearl",
-        api_path: "diamond-pearl"
+        api_path: "diamond-pearl",
+        generation: "generation-iv",
+        versions: ["diamond", "pearl"]
     },
     {
         name: "Platinum",
-        api_path: "platinum"
+        api_path: "platinum",
+        generation: "generation-iv",
+        versions: ["platinum"]
     },
     {
         name: "HeartGold / SoulSilver",
-        api_path: "heartgold-soulsilver"
+        api_path: "heartgold-soulsilver",
+        generation: "generation-iv",
+        versions: ["heartgold", "soulsilver"]
     },
     {
         name: "Black / White",
-        api_path: "black-white"
+        api_path: "black-white",
+        generation: "generation-v",
+        versions: ["black", "white"]
     },
     {
         name: "Black 2 / White 2",
-        api_path: "black-2-white-2"
+        api_path: "black-2-white-2",
+        generation: "generation-v",
+        versions: ["black-2", "white-2"]
     },
     {
         name: "X / Y",
-        api_path: "x-y"
+        api_path: "x-y",
+        generation: "generation-vi",
+        versions: ["x", "y"]
     },
     {
         name: "Omega Ruby / Alpha Sapphire",
-        api_path: "omega-ruby-alpha-sapphire"
+        api_path: "omega-ruby-alpha-sapphire",
+        generation: "generation-vi",
+        versions: ["omega-ruby", "alpha-sapphire"]
     },
     {
         name: "Sun / Moon",
-        api_path: "sun-moon"
+        api_path: "sun-moon",
+        generation: "generation-vii",
+        versions: ["sun", "moon"]
     },
     {
         name: "Ultra Sun / Ultra Moon",
-        api_path: "ultra-sun-ultra-moon"
+        api_path: "ultra-sun-ultra-moon",
+        generation: "generation-vii",
+        versions: ["ultra-sun", "ultra-moon"]
     },
     {
         name: "Let's Go Pikachu / Let's Go Eevee",
-        api_path: "lets-go-pikachu-lets-go-eevee"
+        api_path: "lets-go-pikachu-lets-go-eevee",
+        generation: "generation-vii",
+        versions: ["lets-go-pikachu", "lets-go-eevee"]
     },
     {
         name: "Sword / Shield",
-        api_path: "sword-shield"
+        api_path: "sword-shield",
+        generation: "generation-viii",
+        versions: ["sword", "shield"]
     },
     {
         name: "Brilliant Diamond / Shining Pearl",
-        api_path: "brilliant-diamond-shining-pearl"
+        api_path: "brilliant-diamond-shining-pearl",
+        generation: "generation-viii",
+        versions: ["brilliant-diamond", "shining-pearl"]
     },
     {
         name: "Legends: Arceus",
-        api_path: "legends-arceus"
+        api_path: "legends-arceus",
+        generation: "generation-viii",
+        versions: ["legends-arceus"]
     },
     {
         name: "Scarlet / Violet",
-        api_path: "scarlet-violet"
+        api_path: "scarlet-violet",
+        generation: "generation-ix",
+        versions: ["scarlet", "violet"]
     }
 ];
 
