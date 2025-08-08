d411471dee3eefbede45e93b2ca9f07f93369f02
odisfm <102568741+odisfm@users.noreply.github.com>
2025-08-08 12:25:51 +1000

    use pokeapi-wrapper cache

--- client/src/utils/dex.tsx
+++ client/src/utils/dex.tsx
@@ -1,3 +1,8 @@
 import {Pokedex} from "pokeapi-js-wrapper";
 
-export default new Pokedex()
+export default new Pokedex({
+    protocol: "https",
+    versionPath: "/api/v2/",
+    cache: true,
+    timeout: 5 * 1000, // 5s
+})
