5af414cfa14f677bd54ff3aec4e16e6b7e3e4dc5
odisfm <102568741+odisfm@users.noreply.github.com>
2025-08-11 08:46:41 +1000

    style

--- client/src/components/DexList/DexView.tsx
+++ client/src/components/DexList/DexView.tsx
@@ -47,7 +47,7 @@ export function DexView(): ReactElement | null {
 
                 <form onSubmit={(e) => {searchMon(e)}} className={"flex gap-2 overflow-hidden rounded-md items-stretch bg-slate-700 text-white p-2"}>
                     <input ref={monSearchInput} name="mon-name" className={"bg-white p-2 rounded-md text-black"} placeholder={"search for a Pokémon"} />
-                    <button type="submit" className={"fill-white [&_svg]:size-6 hover:bg-slate-800 px-2 py-1 rounded-lg"}><SearchIcon/></button>
+                    <button type="submit" className={"cursor-pointer fill-white [&_svg]:size-6 hover:bg-slate-800 px-2 py-1 rounded-lg"}><SearchIcon/></button>
                 </form>
                 <button onClick={randomMon} className={"group flex gap-3 bg-slate-700 hover:bg-slate-800 px-6 py-4 rounded-md text-white cursor-pointer self-center mb-2 items-center"}>
                     <div className="size-5 fill-white group-hover:animate-[spin_250ms_ease-in-out]"><DiceIcon/></div>Random Pokémon!            </button>
