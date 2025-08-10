35766289fc662f5f9661ea69b13e7dbd280bc5f2
odisfm <102568741+odisfm@users.noreply.github.com>
2025-08-11 08:56:40 +1000

    add id prop

--- client/src/components/MonViewer/BattleDetails/Card.tsx
+++ client/src/components/MonViewer/BattleDetails/Card.tsx
@@ -1,8 +1,8 @@
 import type {ReactElement} from "react";
 
-export default function Card({children, extraStyles=""}: {children: React.ReactNode, extraStyles: string}): ReactElement {
+export default function Card({children, extraStyles="", id}: {children: React.ReactNode, extraStyles: string | null, id: string | null}): ReactElement {
     return (
-        <div className={`flex flex-col gap-3 items-center bg-white px-8 py-5 [&_h3]:font-bold [&_h3]:text-3xl md:self-start ${extraStyles}`}>
+        <div id={id || undefined} className={`flex flex-col gap-3 items-center bg-white px-8 py-5 [&_h3]:font-bold [&_h3]:text-3xl md:self-start ${extraStyles}`}>
             {children}
         </div>
     )
