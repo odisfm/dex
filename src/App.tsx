f659eb6a5021fb64883b598a55b9c3b69754d658
odisfm <102568741+odisfm@users.noreply.github.com>
2025-08-11 13:01:10 +1000

    fix 404 for deeplink

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,54 @@
 import VersionPicker from "./components/VersionPicker.tsx";
-import {Link, Outlet} from "react-router-dom";
+import {Link, Outlet, useNavigate} from "react-router-dom";
 import LanguagePicker from "./components/LanguagePicker.tsx";
 // @ts-expect-error "path"
 import GithubIcon from "./icon/github.svg?react"
+import {useEffect} from "react";
 
 function App() {
+    const navigate = useNavigate();
 
-    return (
-        <>
-            <header
-                className={"z-100 sticky top-0 p-1 text-white gap-2 md:gap-10 bg-slate-800 flex items-center max-w-screen"}>
-                <a href={"/#"} className={"p-2 md:px-3 lg:px-5 rounded-md hover:bg-slate-700"}><h1
-                    className={"text-3xl font-black"}>Dex</h1></a>
-                <div className={"flex gap-3 w-full"}>
-                    <VersionPicker/>
-                    <LanguagePicker/>
-                </div>
-            </header>
-            <div className={"relative h-full w-full"}>
-                <main
-                    className={"bg-gray-100 flex flex-col gap-3 h-full w-full min-h-screen items-center py-5 px-3 md:px-10 text-black"}>
-                    <Outlet></Outlet>
-                </main>
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const redirectPath = urlParams.get('redirect');
+
+        if (redirectPath) {
+            window.history.replaceState(null, "", '/');
+            navigate(redirectPath, {replace: true});
+        }
+    }, [navigate]);
+
+
+return (
+    <>
+        <header
+            className={"z-100 sticky top-0 p-1 text-white gap-2 md:gap-10 bg-slate-800 flex items-center max-w-screen"}>
+            <a href={"/#"} className={"p-2 md:px-3 lg:px-5 rounded-md hover:bg-slate-700"}><h1
+                className={"text-3xl font-black"}>Dex</h1></a>
+            <div className={"flex gap-3 w-full"}>
+                <VersionPicker/>
+                <LanguagePicker/>
             </div>
-            <footer className={"flex bg-slate-800 px-10 py-3 text-white h-max-20 items-center"}>
-                <Link to={"https://github.com/odisfm/dex"} className={"group rounded-md px-3 py-1 hover:bg-slate-900"}>
+        </header>
+        <div className={"relative h-full w-full"}>
+            <main
+                className={"bg-gray-100 flex flex-col gap-3 h-full w-full min-h-screen items-center py-5 px-3 md:px-10 text-black"}>
+                <Outlet></Outlet>
+            </main>
+        </div>
+        <footer className={"flex bg-slate-800 px-10 py-3 text-white h-max-20 items-center"}>
+            <Link to={"https://github.com/odisfm/dex"} className={"group rounded-md px-3 py-1 hover:bg-slate-900"}>
                 <span className={"flex gap-2 fill-white"}>
                     <div className={"size-6 "}><GithubIcon/></div>
                     View source
                 </span>
-                </Link>
-                <span className={"ml-auto"}>Powered by <a className={"font-bold hover:underline"}
-                                                          href={"https://pokeapi.co"}>PokeApi</a></span>
-            </footer>
-        </>
+            </Link>
+            <span className={"ml-auto"}>Powered by <a className={"font-bold hover:underline"}
+                                                      href={"https://pokeapi.co"}>PokeApi</a></span>
+        </footer>
+    </>
 
-    )
+)
 }
 
 export default App
